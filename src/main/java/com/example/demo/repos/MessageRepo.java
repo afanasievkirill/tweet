4d8e170f8781846b87ca8290fa7e013dcf036bbf
afanasievkirill <43038514+afanasievkirill@users.noreply.github.com>
2020-03-30 08:21:22 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.example.demo.repos;
+
+import com.example.demo.model.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+}
