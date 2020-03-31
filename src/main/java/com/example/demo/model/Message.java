71ae8e1d4e680c08ddbcbdb1e774a8ef43956096
afanasievkirill <43038514+afanasievkirill@users.noreply.github.com>
2020-03-31 12:18:13 +0300

    Add images and static

--- src/main/java/com/example/demo/model/Message.java
+++ src/main/java/com/example/demo/model/Message.java
@@ -13,6 +13,7 @@ public class Message {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
+    private String filename;
 
     public Message() {
     }
@@ -59,4 +60,11 @@ public class Message {
         this.tag = tag;
     }
 
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
 }
