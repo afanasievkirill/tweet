f0ec5f22b7d0813ef4483b509cdd418bd4e232bb
afanasievkirill <43038514+afanasievkirill@users.noreply.github.com>
2020-04-01 15:34:30 +0300

    transfer to postgres sql
     transfer to postgres sql

--- src/main/java/com/example/demo/model/Message.java
+++ src/main/java/com/example/demo/model/Message.java
@@ -7,7 +7,7 @@ public class Message {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
     private String text;
     private String tag;
     @ManyToOne(fetch = FetchType.EAGER)
@@ -36,11 +36,11 @@ public class Message {
         this.author = author;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
